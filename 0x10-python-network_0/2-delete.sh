983f3fb7fbb975232088aa7cd2e84d33cf0ebb6d
Hadija-S-Mahmoud <hsmahmoud20@gmail.com>
2024-04-25 13:27:35 -0700

    corrected

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends DELETE request to the URL passed as the first argument,
-# and displays the body of the respons
+# Sends DELETE request to the URL passed as the first argument and displays the body of the respons
 curl -s -X DELETE "${1}"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Send sPOST request to the passed URL,
-# and displays the body of the response
+# Send sPOST request to the passed URL and displays the body of the response
 curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" "${1}"
